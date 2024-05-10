d758a8c5de1ac90c8f2dc09b5e3767be7a837123
vivekgonuguntla <149650228+vivekgonuguntla@users.noreply.github.com>
2024-05-10 14:11:02 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAtC2830wKKqo-9q_wzMzDfvxxKXW19beQ",
+  authDomain: "vivek-curd.firebaseapp.com",
+  databaseURL: "https://vivek-curd-default-rtdb.firebaseio.com",
+  projectId: "vivek-curd",
+  storageBucket: "vivek-curd.appspot.com",
+  messagingSenderId: "975255823076",
+  appId: "1:975255823076:web:5cf0e36b2160d6f31002f1",
+  measurementId: "G-8XDW0ZHZW2"
+};
+// Initialize Firebase
+firebase.initializeApp (firebaseConfig);
